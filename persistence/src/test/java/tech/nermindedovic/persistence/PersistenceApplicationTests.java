2edb3ee0531a95e7c101949e08654218a083bb0d
dedovicnermin <dedovicnermin@outlook.com>
2021-03-22 17:47:37 -0500

    adding persistence package

---
+++ persistence/src/test/java/tech/nermindedovic/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
