31e2661a450172b1e4e03e5e0929625b116f132e
dedovicnermin <dedovicnermin@outlook.com>
2021-03-26 08:25:04 -0500

    Add spring-boot transformer application (app B)

---
+++ transformer/src/test/java/tech/nermindedovic/transformer/TransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
