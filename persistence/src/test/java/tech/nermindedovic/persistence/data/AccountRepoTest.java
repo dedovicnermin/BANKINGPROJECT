832c7fc0d41100f1c7421fe4b166598d6313c07a
dedovicnermin <dedovicnermin@outlook.com>
2021-04-09 21:07:28 -0500

    replace @SpringBootTest with @DataJpaTest

--- persistence/src/test/java/tech/nermindedovic/persistence/data/AccountRepoTest.java
+++ persistence/src/test/java/tech/nermindedovic/persistence/data/AccountRepoTest.java
@@ -1,48 +1,38 @@
 package tech.nermindedovic.persistence.data;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import tech.nermindedovic.persistence.data.entity.Account;
 import tech.nermindedovic.persistence.data.repository.AccountRepository;
-
-
 import java.math.BigDecimal;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
-@DirtiesContext
+
+@DataJpaTest
 class AccountRepoTest {
 
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Autowired
     private AccountRepository repository;
 
 
-
-
     @Test
     void givenAccount_whenSave_thenGetOk() {
         Account account = new Account(1L,1L, "Bob", new BigDecimal("200.00"));
-        repository.save(account);
-        assertEquals(1,repository.count());
-        Account account1 = repository.findById(1L).get();
-        assertEquals("Bob", account1.getUserName());
-        assertEquals(1L, account1.getRoutingNumber());
-
-
+        entityManager.persist(account);
+        Account actual = repository.findById(1L).get();
+        assertEquals(account.getUserName(), actual.getUserName());
+        assertEquals(account.getRoutingNumber(), actual.getRoutingNumber());
+        assertEquals(account.getAccountBalance(), actual.getAccountBalance());
     }
-
-
 
     @Test
     void givenNonExisting_whenRetrieving_emptyOptional_test() {
