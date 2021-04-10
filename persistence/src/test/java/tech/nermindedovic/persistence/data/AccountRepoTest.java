048a074528575589bc0e8331a342b3f157d46742
dedovicnermin <dedovicnermin@outlook.com>
2021-04-09 20:51:20 -0500

    persistence test ci update

--- persistence/src/test/java/tech/nermindedovic/persistence/data/AccountRepoTest.java
+++ persistence/src/test/java/tech/nermindedovic/persistence/data/AccountRepoTest.java
@@ -1,9 +1,14 @@
 package tech.nermindedovic.persistence.data;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tech.nermindedovic.persistence.data.entity.Account;
 import tech.nermindedovic.persistence.data.repository.AccountRepository;
 
@@ -14,21 +19,29 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext
 class AccountRepoTest {
 
 
     @Autowired
     private AccountRepository repository;
 
+
+
+
     @Test
     void givenAccount_whenSave_thenGetOk() {
         Account account = new Account(1L,1L, "Bob", new BigDecimal("200.00"));
         repository.save(account);
-
+        assertEquals(1,repository.count());
         Account account1 = repository.findById(1L).get();
         assertEquals("Bob", account1.getUserName());
-        assertEquals(account.getRoutingNumber(), account1.getRoutingNumber());
+        assertEquals(1L, account1.getRoutingNumber());
+
+
     }
+
 
 
     @Test
