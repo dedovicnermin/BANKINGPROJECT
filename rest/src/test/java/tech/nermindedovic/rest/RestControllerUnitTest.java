9f2073a416d2095d5eeecfe72c53974054822ee7
dedovicnermin <dedovicnermin@outlook.com>
2021-04-14 22:45:04 -0500

    removing unused imports

--- rest/src/test/java/tech/nermindedovic/rest/RestControllerUnitTest.java
+++ rest/src/test/java/tech/nermindedovic/rest/RestControllerUnitTest.java
@@ -4,16 +4,11 @@ package tech.nermindedovic.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import tech.nermindedovic.rest.api.RestAPI;
 
@@ -27,10 +22,8 @@ import tech.nermindedovic.rest.kafka.transfer.TransferFundsProducer;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
