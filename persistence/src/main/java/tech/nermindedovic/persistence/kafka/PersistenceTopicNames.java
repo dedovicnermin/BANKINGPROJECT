dfd0b86a11f01872ed378716ce3eec42e29f86f0
dedovicnermin <dedovicnermin@outlook.com>
2021-04-26 14:39:10 -0500

    updating persistence topic names

--- persistence/src/main/java/tech/nermindedovic/persistence/kafka/PersistenceTopicNames.java
+++ persistence/src/main/java/tech/nermindedovic/persistence/kafka/PersistenceTopicNames.java
@@ -3,8 +3,13 @@ package tech.nermindedovic.persistence.kafka;
 public final class PersistenceTopicNames {
     private PersistenceTopicNames() {}
 
-    public static final String INBOUND_TRANSFER_REQUEST = "funds.transfer.request";
-    public static final String INBOUND_BALANCE_REQUEST = "balance.update.request";
+    public static final String INBOUND_TRANSFER_REQUEST = "funds.transfer.111";
+    public static final String INBOUND_TRANSFER_VALIDATION = "funds.validate.111";
+    public static final String INBOUND_TRANSFER_SINGLE_USER = "funds.transfer.single.111";
+    public static final String INBOUND_BALANCE_REQUEST = "balance.update.request.111";
     public static final String OUTBOUND_TRANSFER_ERRORS = "funds.transfer.error";
+
+    public static final String OUTBOUND_ROUTER_VALIDATION = "router.validate.transfer";
+
 
 }
