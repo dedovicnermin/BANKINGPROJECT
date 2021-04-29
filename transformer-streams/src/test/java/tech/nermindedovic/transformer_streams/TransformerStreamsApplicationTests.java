ddd32da2d694acd1279a8e72cfd848500c867df5
dedovicnermin <dedovicnermin@outlook.com>
2021-04-28 19:16:26 -0500

    transformer app attempting to connect to localhost:9092 fix

--- transformer-streams/src/test/java/tech/nermindedovic/transformer_streams/TransformerStreamsApplicationTests.java
+++ transformer-streams/src/test/java/tech/nermindedovic/transformer_streams/TransformerStreamsApplicationTests.java
@@ -3,9 +3,7 @@ package tech.nermindedovic.transformer_streams;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
