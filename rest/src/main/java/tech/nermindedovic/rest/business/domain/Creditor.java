adbfa6f91488b20d3ec56a97b655b6e004c71400
dedovicnermin <dedovicnermin@outlook.com>
2021-04-30 12:40:45 -0500

    removing @NoArgsConstructor and adding lombok.config

--- rest/src/main/java/tech/nermindedovic/rest/business/domain/Creditor.java
+++ rest/src/main/java/tech/nermindedovic/rest/business/domain/Creditor.java
@@ -2,13 +2,11 @@ package tech.nermindedovic.rest.business.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Creditor {
 
     @NotNull
