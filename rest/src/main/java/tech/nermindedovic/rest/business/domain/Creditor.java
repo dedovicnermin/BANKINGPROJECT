04f807f6b9ffad87a0d028ce5ed5ccecf5b79409
dedovicnermin <dedovicnermin@outlook.com>
2021-04-30 12:47:27 -0500

    adding back @NoArgsConstructor

--- rest/src/main/java/tech/nermindedovic/rest/business/domain/Creditor.java
+++ rest/src/main/java/tech/nermindedovic/rest/business/domain/Creditor.java
@@ -2,10 +2,12 @@ package tech.nermindedovic.rest.business.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Creditor {
 
--- rest/src/main/java/tech/nermindedovic/rest/business/domain/Debtor.java
+++ rest/src/main/java/tech/nermindedovic/rest/business/domain/Debtor.java
@@ -2,8 +2,10 @@ package tech.nermindedovic.rest.business.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Debtor {
     private long accountNumber;
