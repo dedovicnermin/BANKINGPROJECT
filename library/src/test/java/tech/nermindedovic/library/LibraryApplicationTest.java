b3160ce7916d0656561e9d6daa2e5ee9033cae10
dedovicnermin <dedovicnermin@outlook.com>
2021-05-02 13:42:40 -0500

    library tests

--- library/src/test/java/tech/nermindedovic/library/LibraryApplicationTest.java
+++ library/src/test/java/tech/nermindedovic/library/LibraryApplicationTest.java
@@ -13,7 +13,132 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LibraryApplicationTest {
 
     @Test
-    void pojos() {
+    void transferValidation_test() {
+        long messageId = 1;
+        BigDecimal amount = BigDecimal.TEN;
+        int leg = 1;
+        String transferMessage = "Mock TransferMessage XML";
+        Creditor creditor = new Creditor(44L, 222L);
+        Debtor debtor = new Debtor(11L, 111L);
+
+
+        TransferValidation transferValidation = new TransferValidation();
+
+        transferValidation.setMessageId(messageId);
+        transferValidation.setAmount(amount);
+        transferValidation.setCurrentLeg(leg);
+        transferValidation.setTransferMessage(transferMessage);
+        transferValidation.setCreditorAccount(creditor);
+        transferValidation.setDebtorAccount(debtor);
+
+        TransferValidation validationAllArgs = new TransferValidation(messageId, amount, leg, transferMessage, debtor, creditor);
+
+        Assertions.assertAll(
+                () -> assertThat(validationAllArgs.getMessageId()).isEqualTo(messageId),
+                () -> assertThat(validationAllArgs.getAmount()).isEqualTo(amount),
+                () -> assertThat(validationAllArgs.getCurrentLeg()).isEqualTo(leg),
+                () -> assertThat(validationAllArgs.getTransferMessage()).isEqualTo(transferMessage),
+                () -> assertThat(validationAllArgs.getCreditorAccount()).isEqualTo(creditor),
+                () -> assertThat(validationAllArgs.getDebtorAccount()).isEqualTo(debtor),
+
+                () -> assertThat(transferValidation.getMessageId()).isEqualTo(messageId),
+                () -> assertThat(transferValidation.getAmount()).isEqualTo(amount),
+                () -> assertThat(transferValidation.getCurrentLeg()).isEqualTo(leg),
+                () -> assertThat(transferValidation.getTransferMessage()).isEqualTo(transferMessage),
+                () -> assertThat(transferValidation.getCreditorAccount()).isEqualTo(creditor),
+                () -> assertThat(transferValidation.getDebtorAccount()).isEqualTo(debtor)
+        );
+
+    }
+
+
+    @Test
+    void transferMessage_test() {
+        long messageId = 100;
+        Creditor creditor = new Creditor(44L, 222L);
+        Debtor debtor = new Debtor(11L, 111L);
+        LocalDate date = LocalDate.now();
+        BigDecimal amount = BigDecimal.TEN;
+        String memo = "A funds transfer memo";
+
+        TransferMessage transferMessageAllArgs = new TransferMessage(messageId, creditor, debtor, date, amount, memo);
+        TransferMessage transferMessage = new TransferMessage();
+        transferMessage.setMessageId(messageId);
+        transferMessage.setCreditor(creditor);
+        transferMessage.setDebtor(debtor);
+        transferMessage.setDate(date);
+        transferMessage.setAmount(amount);
+        transferMessage.setMemo(memo);
+
+        Assertions.assertAll(
+                () -> assertThat(transferMessageAllArgs.getMessageId()).isEqualTo(messageId),
+                () -> assertThat(transferMessageAllArgs.getCreditor()).isEqualTo(creditor),
+                () -> assertThat(transferMessageAllArgs.getDebtor()).isEqualTo(debtor),
+                () -> assertThat(transferMessageAllArgs.getDate()).isEqualTo(date),
+                () -> assertThat(transferMessageAllArgs.getAmount()).isEqualTo(amount),
+                () -> assertThat(transferMessageAllArgs.getMemo()).isEqualTo(memo),
+
+                () -> assertThat(transferMessage.getMessageId()).isEqualTo(messageId),
+                () -> assertThat(transferMessage.getCreditor()).isEqualTo(creditor),
+                () -> assertThat(transferMessage.getDebtor()).isEqualTo(debtor),
+                () -> assertThat(transferMessage.getDate()).isEqualTo(date),
+                () -> assertThat(transferMessage.getAmount()).isEqualTo(amount),
+                () -> assertThat(transferMessage.getMemo()).isEqualTo(memo)
+        );
+    }
+
+    @Test
+    void debtor_creditor_test() {
+        long debtorAN = 99L, debtorRN = 111L;
+        long creditorAN = 44L, creditorRN = 222L;
+
+
+        Debtor debtor = new Debtor();
+        debtor.setAccountNumber(debtorAN);
+        debtor.setRoutingNumber(debtorRN);
+
+        Creditor creditor = new Creditor();
+        creditor.setAccountNumber(creditorAN);
+        creditor.setRoutingNumber(creditorRN);
+
+        Assertions.assertAll(
+                () -> assertThat(debtor.getAccountNumber()).isEqualTo(debtorAN),
+                () -> assertThat(debtor.getRoutingNumber()).isEqualTo(debtorRN),
+                () -> assertThat(creditor.getAccountNumber()).isEqualTo(creditorAN),
+                () -> assertThat(creditor.getRoutingNumber()).isEqualTo(creditorRN)
+        );
+    }
+
+
+    @Test
+    void balanceMessage_test() {
+        long accountNumber = 38383, routingNumber = 33333333;
+        String balance = "100.00";
+
+        BalanceMessage balanceMessage = new BalanceMessage();
+        balanceMessage.setAccountNumber(accountNumber);
+        balanceMessage.setRoutingNumber(routingNumber);
+        balanceMessage.setBalance(balance);
+        balanceMessage.setErrors(true);
+        BalanceMessage balanceMessageAllArgs = new BalanceMessage(accountNumber, routingNumber, balance, false);
+
+
+        Assertions.assertAll(
+                () -> assertThat(balanceMessage.getAccountNumber()).isEqualTo(accountNumber),
+                () -> assertThat(balanceMessage.getRoutingNumber()).isEqualTo(routingNumber),
+                () -> assertThat(balanceMessage.getBalance()).isEqualTo(balance),
+                () -> assertThat(balanceMessage.getErrors()).isTrue(),
+
+
+                () -> assertThat(balanceMessageAllArgs.getAccountNumber()).isEqualTo(accountNumber),
+                () -> assertThat(balanceMessageAllArgs.getRoutingNumber()).isEqualTo(routingNumber),
+                () -> assertThat(balanceMessageAllArgs.getBalance()).isEqualTo(balance),
+                () -> assertThat(balanceMessageAllArgs.getErrors()).isFalse()
+        );
+    }
+
+    @Test
+    void pojo_test() {
         BalanceMessage balanceMessage = new BalanceMessage(1L, 222L, "", false);
         Creditor creditor = new Creditor(44L, 222L);
         Debtor debtor = new Debtor(11L, 111L);
@@ -42,10 +167,6 @@ class LibraryApplicationTest {
                 () -> assertThat(balanceMessage.getErrors()).isFalse(),
                 () -> assertThat(validation.getAmount()).isEqualTo(transferMessage.getAmount())
         );
-
-
-
-
     }
 
 }
