a39471afa50431b8804a7835bbce7d1bfc897b30
dedovicnermin <dedovicnermin@outlook.com>
2021-05-03 09:25:44 -0500

    commit prior to refactoring persistence/router-streams for transfer status metric bug

--- rest/src/main/java/tech/nermindedovic/rest/api/WebClientConfig.java
+++ rest/src/main/java/tech/nermindedovic/rest/api/WebClientConfig.java
@@ -1,6 +1,7 @@
 package tech.nermindedovic.rest.api;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,9 +9,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${router-endpoint:localhost:8082}")
+    private String routerEndpoint;
+
     @Bean
     public WebClient routerApiClient() {
-        return WebClient.create("http://localhost:8084/transfer");
+        return WebClient.create("http://" + routerEndpoint + "/transfer");
     }
 
 }
