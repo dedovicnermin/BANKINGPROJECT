6facafc2e32f9bf63c4f64b1574400df41b8d93e
dedovicnermin <dedovicnermin@outlook.com>
2021-05-05 19:53:40 -0500

    using yaml created in swagger editor. localhost:8080/swagger-ui/ endpoint

--- rest/src/main/java/tech/nermindedovic/rest/api/SpringFoxConfig.java
+++ rest/src/main/java/tech/nermindedovic/rest/api/SpringFoxConfig.java
@@ -1,17 +1,23 @@
 package tech.nermindedovic.rest.api;
 
 
-import com.google.common.base.Predicate;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 
 @Configuration
@@ -29,6 +35,21 @@ public class SpringFoxConfig {
 
     private Predicate<RequestHandler> handlerPredicate() {
         return RequestHandlerSelectors.withClassAnnotation(RestController.class);
+    }
+
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider() {
+        return () -> {
+            SwaggerResource wsResource = new SwaggerResource();
+            wsResource.setName("BANKING DEFINITION");
+            wsResource.setSwaggerVersion("3.0");
+            wsResource.setLocation("/openapi.yaml");
+            List<SwaggerResource> resources = new ArrayList<>();
+            resources.add(wsResource);
+            return resources;
+        };
     }
 
 
