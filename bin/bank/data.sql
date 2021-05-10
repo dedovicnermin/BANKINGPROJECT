dde556eb7243c8d03e29594001d3124b7c095b86
dedovicnermin <dedovicnermin@outlook.com>
2021-05-10 17:02:45 -0500

    table changes required. incremental id on transaction. created_at / updated_at for accounts.

--- bin/bank/data.sql
+++ bin/bank/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO accounts (account_number, balance, routing_number, user_name) VALUES (1, 1000.00, 111, 'USER1');
-INSERT INTO accounts (account_number, balance, routing_number, user_name) VALUES (2, 2000.00, 111, 'USER2');
-INSERT INTO accounts (account_number, balance, routing_number, user_name) VALUES (3, 3000.00, 111, 'USER3');
-INSERT INTO accounts (account_number, balance, routing_number, user_name) VALUES (4, 4000.00, 111, 'USER4');
-INSERT INTO accounts (account_number, balance, routing_number, user_name) VALUES (5, 5000.00, 111, 'USER5');
+INSERT INTO accounts (account_number, balance, routing_number, user_name, created_at, updated_at) VALUES (1, 1000.00, 111, 'USER1', current_timestamp , current_timestamp );
+INSERT INTO accounts (account_number, balance, routing_number, user_name, created_at, updated_at) VALUES (2, 2000.00, 111, 'USER2', current_timestamp , current_timestamp );
+INSERT INTO accounts (account_number, balance, routing_number, user_name, created_at, updated_at) VALUES (3, 3000.00, 111, 'USER3', current_timestamp , current_timestamp );
+INSERT INTO accounts (account_number, balance, routing_number, user_name, created_at, updated_at) VALUES (4, 4000.00, 111, 'USER4', current_timestamp , current_timestamp );
+INSERT INTO accounts (account_number, balance, routing_number, user_name, created_at, updated_at) VALUES (5, 5000.00, 111, 'USER5', current_timestamp , current_timestamp );
 
--- bin/bank/schema.sql
+++ bin/bank/schema.sql
@@ -2,16 +2,19 @@
 
 CREATE TABLE accounts (
                           account_number bigint PRIMARY KEY,
-                          balance numeric(10,2),
                           routing_number bigint,
-                          user_name character varying(255)
+                          user_name character varying(255),
+                          balance numeric(10,2),
+                          created_at timestamp without time zone NOT NULL default CURRENT_TIMESTAMP,
+                          updated_at timestamp without time zone NOT NULL default CURRENT_TIMESTAMP
 );
 -- CREATE UNIQUE INDEX accounts_pkey ON accounts(account_number int8_ops);
 
 
 
 CREATE TABLE transactions (
-                              transaction_id bigint PRIMARY KEY,
+                              id serial PRIMARY KEY,
+                              transaction_id bigint,
                               amount numeric(10,2),
                               creditor_account bigint,
                               date timestamp without time zone,
