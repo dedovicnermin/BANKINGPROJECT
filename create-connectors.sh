f8328a61f8b38563285b22f254a9358b48bc25f2
dedovicnermin <dedovicnermin@outlook.com>
2021-05-10 21:05:46 -0500

    bank2 connectors and clean up

--- create-connectors.sh
+++ create-connectors.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 echo "====================="
-echo "Creating connectors!"
+echo "Creating connectors! "
 echo "======================"
+
+
+printf "\n===== BANK1 config - accounts =====\n"
 
 echo
 curl -i -X POST http://localhost:8083/connectors \
@@ -10,7 +13,11 @@ curl -i -X POST http://localhost:8083/connectors \
   -H 'Accept: application/json' \
   -d @bin/bank/jdbc-source-accounts.json
 
+printf "\n===== Completed (1/4) =====\n"
 
+
+
+printf "\n===== BANK1 config - transactions =====\n"
 
 echo
 curl -i -X POST http://localhost:8083/connectors \
@@ -18,6 +25,34 @@ curl -i -X POST http://localhost:8083/connectors \
   -H 'Accept: application/json' \
   -d @bin/bank/jdbc-source-transactions.json
 
-echo "========"
+printf "\n===== Completed (2/4) =====\n"
+
+
+#########################################################
+
+
+printf "\n===== BANK2 config - accounts =====\n"
+
+echo
+curl -i -X POST http://localhost:8083/connectors \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -d @bin/bank2/jdbc-source-accounts.json
+
+printf "\n===== Completed (3/4) =====\n"
+
+
+
+printf "\n===== BANK2 config - transactions =====\n"
+
+curl -i -X POST http://localhost:8083/connectors \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -d @bin/bank2/jdbc-source-transactions.json
+
+printf "\n===== Completed (4/4) =====\n"
+
+
+printf "\n========"
 echo "Done!"
-echo "========"
+printf "========\n"
