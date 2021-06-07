bef237c2f5e4ee5bf48dafc9fff12691128fc662
dedovicnermin <dedovicnermin@outlook.com>
2021-06-07 09:01:06 -0500

    transformer-streams: reusing classes found in library

--- transformer-streams/src/main/java/tech/nermindedovic/transformer_streams/kafka/config/serde/json/CustomSerdes.java
+++ transformer-streams/src/main/java/tech/nermindedovic/transformer_streams/kafka/config/serde/json/CustomSerdes.java
@@ -1,6 +1,8 @@
 package tech.nermindedovic.transformer_streams.kafka.config.serde.json;
 
 import org.apache.kafka.common.serialization.Serdes;
+import tech.nermindedovic.library.kafka.JsonDeserializer;
+import tech.nermindedovic.library.kafka.JsonSerializer;
 import tech.nermindedovic.library.pojos.BalanceMessage;
 
 public final class CustomSerdes {
