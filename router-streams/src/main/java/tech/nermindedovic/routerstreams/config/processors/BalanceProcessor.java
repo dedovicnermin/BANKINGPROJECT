8bed1d39e0110211369e31b71fee3956c9be6161
dedovicnermin <dedovicnermin@outlook.com>
2021-06-06 19:41:05 -0500

    router streams : adding documentation to balanceProcessor

--- router-streams/src/main/java/tech/nermindedovic/routerstreams/config/processors/BalanceProcessor.java
+++ router-streams/src/main/java/tech/nermindedovic/routerstreams/config/processors/BalanceProcessor.java
@@ -3,6 +3,7 @@ package tech.nermindedovic.routerstreams.config.processors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Predicate;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tech.nermindedovic.routerstreams.utils.BalanceMessageParser;
@@ -12,14 +13,15 @@ import tech.nermindedovic.routerstreams.utils.BalanceMessageParser;
 import java.util.function.Function;
 
 @Configuration
+@EnableAutoConfiguration
 @Slf4j
 public class BalanceProcessor {
 
     //  TESTED : 👍🏼
 
-    public static final Predicate<String, String> isRoute111 = (key,val) -> key.equals("111");
-    public static final Predicate<String, String> isRoute222 = (key,val) -> key.equals("222");
-    public static final Predicate<String, String> isUnknownRoute = (key, val) -> true;
+    public final Predicate<String, String> isRoute111 = (key,val) -> key.equals("111");
+    public final Predicate<String, String> isRoute222 = (key,val) -> key.equals("222");
+    public final Predicate<String, String> isUnknownRoute = (key, val) -> true;
 
     private final BalanceMessageParser parser;
     public BalanceProcessor(final BalanceMessageParser balanceMessageParser) {
@@ -27,7 +29,15 @@ public class BalanceProcessor {
     }
 
 
-
+    /**
+     * IN: balance.update.request
+     * OUT:
+     *    [0] - balance.update.request.111
+     *    [1] - balance.update.request.222
+     *    [2] - balance.update.response
+     *
+     * @return KStream[]
+     */
     @Bean
     @SuppressWarnings("unchecked")
     public Function<KStream<String, String>, KStream<String, String>[]> balanceRequestProcessor() {
